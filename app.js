cf32c8da4fdbbaff19beba96b43bef2c1c02aec9
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-29 00:55:41 +0200

    [UPD] databse connected

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const connection = require("./data/db");
 
 app.use(express.json());
 
--- data/db.js
+++ data/db.js
@@ -1,1 +1,21 @@
 const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    port: 3306,
+    password: "",
+    database: "db_blog"
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("databse connected");
+    }
+
+});
+
+module.exports = connection;
