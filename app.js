7962d38824a611212deae0f0fe67966d2c9b9faf
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-29 01:31:50 +0200

    [UPD] router done

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const connection = require("./data/db");
+const blogRouter = require("./routers/blog");
 
 app.use(express.json());
+
+
+app.use("/blog", blogRouter);
 
 app.get("/", (req, res) => {
     console.log("Servere del mio blog");
