b1c412c9e99f0ba9fc7c15e3cb6d78bfa0d03e89
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-29 02:07:36 +0200

    [UPD] milestone 2 done

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,5 +1,17 @@
+const connection = require("../data/db");
+
 const index = ((req, res) => {
-    res.send("elenco dei miei blog");
+    const sql = "SELECT * FROM posts"
+
+    connection.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: "Database query failed" });
+        }
+        else {
+            res.json(results);
+        }
+
+    })
 });
 
 const show = ((req, res) => {
